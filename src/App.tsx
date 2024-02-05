cc8f05ecc94185d898de48a6779d4293f58a96e3
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-05 16:31:13 +0330

    feat: added content structure and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar/NavBar"
+import s from './App.module.sass'
 
 function App() {
 
@@ -6,7 +7,18 @@ function App() {
     <div>
       <NavBar />
       <div className="mainBody">
-
+        <div className="contentSection">
+          <div className={s.textBox}>
+            <h4 className={s.brandText}>Sneaker Company</h4>
+            <h1 className={s.label}>Fall Limited Edition Sneakers</h1>
+            <p className={s.desc}>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+            <h2>$125.00</h2>
+          </div>
+          <div className={s.cartBox}>
+          </div>
+        </div>
+        <div className="pictureSection">
+        </div>
       </div>
     </div>
   )
