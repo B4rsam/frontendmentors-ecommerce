a600e62101ba73d6eb76beb07f7d510e16b301e7
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-07 12:35:29 +0330

    feat: added picture swapping

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,22 +10,22 @@ interface IButton extends HTMLAttributes<HTMLButtonElement> {
     type: btnTypes;
 }
 
-const Button : FC<IButton>= ({children, className, type, src}) => {
+const Button : FC<IButton>= ({children, className, type, src, ...other}) => {
     const handleType = () => {
         switch(type) {
             case "icon":
-                return <button className={`${s.button} ${s.icon} ${className}`}><img src={cart}/></button>
+                return <button {...other} className={`${s.button} ${s.icon} ${className}`}><img src={cart}/></button>
             case "picture":
-                return <button className={`${s.button} ${s.picture} ${className}`}><img src={src}/></button>
+                return <button {...other} className={`${s.button} ${s.picture} ${className}`}><img src={src}/></button>
             case "normal":
                 return (
                     <div className={className}>
                         <img src={cart} className={s.btnImg}/>
-                        <button className={`${s.button} ${s[type]} ${className}`}>{children}</button>
+                        <button {...other} className={`${s.button} ${s[type]} ${className}`}>{children}</button>
                     </div>
                     )
             default:
-                return <button className={`${s.button} ${s[type]} ${className}`}>{children}</button>
+                return <button {...other} className={`${s.button} ${s[type]} ${className}`}>{children}</button>
 
         }    
     }
--- src/components/Pictures/Pictures.tsx
+++ src/components/Pictures/Pictures.tsx
@@ -1,20 +1,30 @@
 import s from './pictures.module.sass'
 import prod1 from '../../assets/products/image-product-1.jpg'
+import prod2 from '../../assets/products/image-product-2.jpg'
+import prod3 from '../../assets/products/image-product-3.jpg'
+import prod4 from '../../assets/products/image-product-4.jpg'
 import thumb1 from '../../assets/products/image-product-1-thumbnail.jpg'
 import thumb2 from '../../assets/products/image-product-2-thumbnail.jpg'
 import thumb3 from '../../assets/products/image-product-3-thumbnail.jpg'
 import thumb4 from '../../assets/products/image-product-4-thumbnail.jpg'
 import Button from '../Button/Button'
+import { ReactNode, useState } from 'react'
 
 const PictureSection = () => {
+    const [currentPic, setPic] = useState(prod1)
+
+    const handlePicChange = (input : any) => {
+        setPic(input)
+    }
+
     return (
         <div className={s.picSection}>
-            <Button type="picture" className={s.mainPic} src={prod1}/>
+            <Button type="picture" className={s.mainPic} src={currentPic}/>
             <div className={s.thumbs}>
-                <Button type="picture" className={s.thumbnails} src={thumb1}/>
-                <Button type="picture" className={s.thumbnails} src={thumb2}/>
-                <Button type="picture" className={s.thumbnails} src={thumb3}/>
-                <Button type="picture" className={s.thumbnails} src={thumb4}/>
+                <Button type="picture" className={s.thumbnails} src={thumb1} onClick={() => handlePicChange(prod1)}/>
+                <Button type="picture" className={s.thumbnails} src={thumb2} onClick={() => handlePicChange(prod2)}/>
+                <Button type="picture" className={s.thumbnails} src={thumb3} onClick={() => handlePicChange(prod3)}/>
+                <Button type="picture" className={s.thumbnails} src={thumb4} onClick={() => handlePicChange(prod4)}/>
             </div>
         </div>
     )
