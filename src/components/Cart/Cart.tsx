91b9893c922e58dca27fa7e82430010bbbdf8769
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-12 14:01:23 +0330

    feat: product count now works

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,14 +2,33 @@ import s from './cart.module.sass'
 import Button from '../Button/Button'
 import plus from '../../assets/icons/icon-plus.svg'
 import minus from '../../assets/icons/icon-minus.svg'
+import { useState } from 'react';
+
+type quantityAction = "add" | "subtract";
 
 const CartSection = () => {
+    const [productCount, setCount] = useState(0)
+
+    const handleQuantity = (action : quantityAction) => {
+        switch(action) {
+            case "add":
+                setCount(productCount + 1)
+                break;
+            case "subtract":
+                console.log(productCount)
+                if (productCount > 0) {
+                    setCount(productCount - 1)
+                }
+                break;
+        }
+    }
+
     return (
         <div className={s.cartBody}>
             <div className={s.quantitySection}>
-                <Button type="picture" className={`${s.countBtn} ${s.minus}`} src={minus}/>
-                <div id="quantity" className={s.countNum}>0</div>
-                <Button type="picture" className={`${s.countBtn} ${s.plus}`} src={plus}/>
+                <Button type="picture" className={`${s.countBtn} ${s.minus}`} src={minus} onClick={() => handleQuantity('subtract')}/>
+                <div id="quantity" className={s.countNum}>{productCount}</div>
+                <Button type="picture" className={`${s.countBtn} ${s.plus}`} src={plus} onClick={() => handleQuantity('add')}/>
             </div>
             <Button type="normal" children={`Add to Cart`} className={s.cartBtn}/>
         </div>
