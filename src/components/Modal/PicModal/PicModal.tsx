30cf40fff51284f68c9de81a083296e3010d6de7
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-12 12:41:39 +0330

    feat: added picModal switcher dummy

--- src/components/Modal/PicModal/PicModal.tsx
+++ src/components/Modal/PicModal/PicModal.tsx
@@ -5,7 +5,12 @@ import thumb1 from '../../../assets/products/image-product-1-thumbnail.jpg'
 import thumb2 from '../../../assets/products/image-product-2-thumbnail.jpg'
 import thumb3 from '../../../assets/products/image-product-3-thumbnail.jpg'
 import thumb4 from '../../../assets/products/image-product-4-thumbnail.jpg'
+import prod1 from '../../../assets/products/image-product-1.jpg'
+import prod2 from '../../../assets/products/image-product-2.jpg'
+import prod3 from '../../../assets/products/image-product-3.jpg'
+import prod4 from '../../../assets/products/image-product-4.jpg'
 
+type direction = 'left' | 'right';
 interface IPicModal extends HTMLAttributes<HTMLDivElement> {
     focusPicture: any
     onExit: () => void
@@ -13,14 +18,24 @@ interface IPicModal extends HTMLAttributes<HTMLDivElement> {
 
 const PicModal : FC<IPicModal>= ({focusPicture, onExit}) => {
     const [focusPic, setPic] = useState(focusPicture)
+    const picList = [prod1, prod2, prod3, prod4]
+
+    const handleSwitch = (direction : direction) => {
+        if (direction === 'left') {
+            console.log(direction)
+        }
+        else {
+            console.log(direction)
+        }
+    }
 
     return (
-        <div className={s.modalWrapper}>
-            <div className={s.modalBody}>
+        <div className={s.modalWrapper} >
+            <div className={s.modalBody} >
                 <Button type="icon" icon="close" className={s.closeBtn} onClick={onExit}/>
                 <img src={focusPic} className={s.focusPicture}/>
-                <Button type="icon" icon="back" className={`${s.switcher} switcher ${s.left}`}/>
-                <Button type="icon" icon="next" className={`${s.switcher} switcher ${s.right}`}/>
+                <Button type="icon" icon="back" className={`${s.switcher} switcher ${s.left}`} onClick={() => handleSwitch('left')}/>
+                <Button type="icon" icon="next" className={`${s.switcher} switcher ${s.right}`} onClick={() => handleSwitch('right')}/>
                 <div className={s.thumbSection}>
                     <img src={thumb1} className={s.thumbs}/>
                     <img src={thumb2} className={s.thumbs}/>
