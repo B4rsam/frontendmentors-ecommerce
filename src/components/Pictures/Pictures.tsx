e27698d6b742322fd6e1ae3d5588339d20efc0d9
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-18 15:12:58 +0330

    feat: added switcher to mobile picture

--- src/components/Pictures/Pictures.tsx
+++ src/components/Pictures/Pictures.tsx
@@ -8,12 +8,13 @@ import thumb2 from '../../assets/products/image-product-2-thumbnail.jpg'
 import thumb3 from '../../assets/products/image-product-3-thumbnail.jpg'
 import thumb4 from '../../assets/products/image-product-4-thumbnail.jpg'
 import Button from '../Button/Button'
-import { useId, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import PicModal from '../Modal/PicModal/PicModal'
 
 const PictureSection = () => {
     const [currentPic, setPic] = useState(prod1)
     const [modal, setModal] = useState(false)
+    const [index, setIndex] = useState(0)
     const picList = [{prod: prod1, thumb: thumb1, id: 0}, {prod: prod2, thumb: thumb2, id: 1}, {prod: prod3, thumb: thumb3, id: 2}, {prod: prod4, thumb: thumb4, id: 3}]
     const pictureList = useMemo(() => picList.map((item, index) => {
         return ( 
@@ -27,10 +28,37 @@ const PictureSection = () => {
         setModal(!modal)
     } 
 
+    const handleSwitch = (direction : any) => {
+        switch(direction) {
+            case 'left':
+                if(index-1 < 0) {
+                    setIndex(picList.length-1)
+                    setPic(picList[picList.length-1].prod)
+                }
+                else {
+                    setIndex(index-1)
+                    setPic(picList[index-1].prod)    
+                }
+                break;
+            case 'right':
+                if(index+1 > picList.length-1) {
+                    setIndex(0)
+                    setPic(picList[0].prod)
+                }
+                else {
+                    setIndex(index+1)
+                    setPic(picList[index+1].prod)    
+                }
+                break;
+        }
+    }
+
     return (
         <div className={s.picSection}>
             {modal ? <PicModal onExit={handleModal} focusPicture={currentPic} picList={picList}/> : null}
+            <Button type="icon" icon="back" className={`${s.switcher} switcher ${s.left}`} onClick={() => handleSwitch('left')}/>
             <Button type="picture" className={s.mainPic} src={currentPic} onClick={handleModal}/>
+            <Button type="icon" icon="next" className={`${s.switcher} switcher ${s.right}`} onClick={() => handleSwitch('right')}/> 
             <form className={s.thumbs}>
                 {pictureList}
             </form>
